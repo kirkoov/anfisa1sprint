7e7cf26ea5c92646672fdcb2d22987745063658b
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-05-21 13:56:21 +0300

    Wrap up by adding '127.0.0.1' to the ALLOWED_HOSTS.

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-oy%f52n92s=%3&b3dh&h)f(b4ag^_z*&3w+ooo!sy-g=449bwn
 
 DEBUG = True
 
-ALLOWED_HOSTS: List[str] = ['localhost', ]
+ALLOWED_HOSTS: List[str] = ['localhost', '127.0.0.1', ]
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
